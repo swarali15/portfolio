4a282153f5310bbd17252e9da6e674ac0ddbdd05
swarali15 <swarali.purandare18@vit.edu>
2020-07-28 11:36:45 +0530

    Updates

---
+++ precache-manifest.684ad3d583beab470b978347caab53aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e53bc991132d736ced70921f51b4d17",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2b44d9f3714d17b8295c",
+    "url": "/portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c4a14d05442ad074292c",
+    "url": "/portfolio/static/css/main.f948ef8f.chunk.css"
+  },
+  {
+    "revision": "2b44d9f3714d17b8295c",
+    "url": "/portfolio/static/js/2.2de20eef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.2de20eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a14d05442ad074292c",
+    "url": "/portfolio/static/js/main.9bc8f3b8.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6b6aef7d5d7d939d75250e8dbe03e775",
+    "url": "/portfolio/static/media/left.6b6aef7d.svg"
+  },
+  {
+    "revision": "ed5b64d955c870718f80b351535e3c80",
+    "url": "/portfolio/static/media/right.ed5b64d9.svg"
+  }
+]);
